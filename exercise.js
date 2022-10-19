feb3b030f13e207cb04a2bed4caa8299877378be
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-10-19 22:03:36 +0300

    init

---
+++ exercise.js
@@ -0,0 +1,11 @@
+// In the exercise.js file, we find a user object. Write a function that saves the user object in localStorage with its 'user' key.
+
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+const saveLocaly = (user) => localStorage.setItem("user", JSON.stringify(user));
+
+saveLocaly(user);
